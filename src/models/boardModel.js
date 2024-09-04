709a60d4812929bbf7dcac24266551eb552b05f9
HoangAnhTuan2902 <hoanganhtuan29022004@gmail.com>
2024-09-04 11:36:47 +0700

    aggregate-mongodb

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,6 +1,9 @@
 import { ObjectId } from 'mongodb';
 import Joi from 'joi';
 import { GET_DB } from '~/config/mongodb';
+import { BOARD_TYPES } from '~/utils/constants';
+import { columnModel } from './columnModel';
+import { cardModel } from './cardModel';
 
 // define Collection (Name & Schema)
 
@@ -9,6 +12,7 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
 	title: Joi.string().required().min(3).max(50).trim().strict(),
 	slug: Joi.string().required().min(3).trim().strict(),
 	description: Joi.string().required().min(3).max(255).trim().strict(),
+	type: Joi.string().valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE).required(),
 	columnOrderIds: Joi.array().items(Joi.string()).default([]),
 	createAt: Joi.date().timestamp('javascript').default(Date.now()),
 	updatedAt: Joi.date().timestamp('javascript').default(null),
@@ -48,10 +52,37 @@ const findOneById = async (id) => {
 // query tổng hợp (aggregate) để lấy toàn bộ columns thuộc về Board
 const getDetails = async (id) => {
 	try {
+		// const result = await GET_DB()
+		// 	.collection(BOARD_COLLECTION_NAME)
+		// 	.findOne({ _id: new ObjectId(id) });
 		const result = await GET_DB()
 			.collection(BOARD_COLLECTION_NAME)
-			.findOne({ _id: new ObjectId(id) });
-		return result;
+			.aggregate([
+				{
+					$match: {
+						_id: new ObjectId(id),
+						_destroy: false,
+					},
+				},
+				{
+					$lookup: {
+						from: columnModel.COLUMN_COLLECTION_NAME,
+						localField: '_id',
+						foreignField: 'boardId',
+						as: 'columns',
+					},
+				},
+				{
+					$lookup: {
+						from: cardModel.CARD_COLLECTION_NAME,
+						localField: '_id',
+						foreignField: 'boardId',
+						as: 'cards',
+					},
+				},
+			])
+			.toArray();
+		return result[0] || {};
 	} catch (error) {
 		throw new Error(error);
 	}
--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -7,11 +7,15 @@
 import Joi from 'joi';
 import { StatusCodes } from 'http-status-codes';
 import ApiError from '~/utils/ApiError';
+import { BOARD_TYPES } from '~/utils/constants';
 
 const createNew = async (req, res, next) => {
 	const correctCondition = Joi.object({
 		title: Joi.string().required().min(3).max(50).trim().strict(),
 		description: Joi.string().required().min(3).max(255).trim().strict(),
+		type: Joi.string()
+			.valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE)
+			.required(),
 	});
 
 	try {
@@ -21,7 +25,10 @@ const createNew = async (req, res, next) => {
 		next();
 	} catch (error) {
 		const errorMessage = new Error(error).message;
-		const customError = new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, errorMessage);
+		const customError = new ApiError(
+			StatusCodes.UNPROCESSABLE_ENTITY,
+			errorMessage,
+		);
 		next(customError);
 	}
 };
